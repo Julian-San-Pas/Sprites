1c07e7afe8b3069f5488c6ce048f0050bba5c36d
Julian-San-Pas <julian.sanchez_pas@uao.edu.co>
2025-09-12 15:49:33 -0500

    Cambio del primer nivel
    Al presionar el boton del menu prototipo lleva al primer nivel base prototipo

---
+++ Cuarto Proyecto/Assets/Scripts/PasoNivel.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PasoNivel : MonoBehaviour
+{
+    public void LoadName()
+    {
+        SceneManager.LoadScene("");
+    }
+    public void LoadNumber()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
